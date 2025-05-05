6c5ca822a71fb3f2b8b5663ff4ac654aeddf4c4b
Kiba <munyontodd@gmail.com>
2025-05-04 18:16:12 -0600

    Fix pfp

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -173,10 +173,10 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/me.png"
+              src="../../images/me.jpg"
               width={500}
               quality={95}
-              formats={['AUTO', 'WEBP', 'AVIF', 'PNG']}
+              formats={['AUTO', 'WEBP', 'AVIF']}
               alt="Headshot"
             />
           </div>
